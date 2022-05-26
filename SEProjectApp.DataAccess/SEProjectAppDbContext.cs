34e576aa39bf6f461c301d2a274605d99ff00da9
IonAndreea-Maria <ion.andreea.u5t@student.ucv.ro>
2022-05-26 10:23:29 +0300

    renamed

---
+++ SEProjectApp.DataAccess/SEProjectAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEProjectApp.DataModule;
+
+namespace SEProjectApp.DataAccess
+{
+    public class SEProjectAppDbContext : DbContext
+    {
+        public SEProjectAppDbContext(DbContextOptions<SEProjectAppDbContext> options) : base(options)
+        { }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+    }
+}
+
+
